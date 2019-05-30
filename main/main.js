f7cee97f5a130f52615145ff60e378a3eddba3d4
chenjt <1148013214@qq.com>
2019-05-30 17:29:58 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,45 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(number) {
+    numLedArray = [
+    ["._.",
+     "|.|",
+     "|_|"],
+    ["...",
+     "..|",
+     "..|"],
+    ["._.",
+     "._|",
+     "|_."],
+    ["._.",
+     "._|",
+     "._|"],
+    ["...",
+     "|_|",
+     "..|"],
+    ["._.",
+     "|_.",
+     "._|"],
+    ["._.",
+     "|_.",
+     "|_|"],
+    ["._.",
+     "..|",
+     "..|"],
+    ["._.",
+     "|_|",
+     "|_|"],
+    ["._.",
+     "|_|",
+     "..|"]
+    ];
+    res = "";
+    number = number.toString();
+    for(i=0;i<3;++i)
+    {
+        for(j=0;j<number.length-1;++j)
+        {
+            res += numLedArray[parseInt(number[j])][i]+" ";
+        }
+        res += numLedArray[parseInt(number[j])][i]+"\n";
+    }
+    return res;
 };
